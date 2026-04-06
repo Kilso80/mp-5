8c161e72dfc811ce7b35a8507a30e245f3f4da84
Kilso80 <fioletarthur@gmail.com>
2026-04-06 18:28:40 +0200

    handle invalid aliases

--- app/r/[id]/page.tsx
+++ app/r/[id]/page.tsx
@@ -1,9 +1,15 @@
 'use server';
 import getLink from "@/lib/getLink";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 export default async function Redirecter({ params }: { params: Promise<{ id: string }> }) {
     const url = await getLink((await params).id);
     
+    if (url === undefined || url === null)
+        return (<>
+            <h2>This URL does not exist</h2>
+            <p>Maybe it has expired?</p>
+        </>);
+
     redirect(url);
 }
